8c1c1ee7e46c0453c3f9c2b34d80be39c47574d9
jarred richards <jarred2828@gmail.com>
2019-03-06 16:32:44 -0500

    Finished-This.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window binding - bound to the window by default.
+* 2. Implicit - Binds to objets when used in methods.
+* 3. New - adds new objects to the array.
+* 4. Explicit - using specific words to call your function.
 *
 * write out a code example of each explanation above
 */
@@ -13,14 +13,34 @@
 
 // code example for Window Binding
 
+// function hello (param) {
+//    console.log(this);
+// }
 // Principle 2
 
 // code example for Implicit Binding
+
+// const arr = function (){
+//    this.name = 'arrName';
+//    this.color = 'arrColor';
+//  };
+ 
 
 // Principle 3
 
 // code example for New Binding
 
+// function Animal (food) {
+//    this.food = food;
+//    this.eat = function() {
+//        console.log ('this animal likes to eat ${this.food}');
+//    }
+// }
+
 // Principle 4
 
 // code example for Explicit Binding
+
+// const arr = function (){
+//    this.call(arrName)
+// }
